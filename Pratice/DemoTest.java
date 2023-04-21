e44411fc05ffe58f0e9d477c5b6616a73b0df5c9
SwethaKB18github <swethasweety.kb@gmail.com>
2023-04-21 20:01:26 +0530

    1st commit

---
+++ Pratice/DemoTest.java
@@ -0,0 +1,14 @@
+package Pratice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void method() 
+	{
+		System.out.println();
+		//pushed nd pulling back
+	}
+
+}
---
+++ src/test/java/Pratice/DemoTest.java
@@ -0,0 +1,16 @@
+package Pratice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void method() 
+	{
+		System.out.println();
+		//pushed nd pulling back
+		//pulled nd pushing back
+		//2nd pull
+	}
+
+}
